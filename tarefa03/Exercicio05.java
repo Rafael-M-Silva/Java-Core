2bf827502f4ac113b656af5e37fe87568d400c3b
Rafael Mauricio da SIlva <rafael_mauricio@hotmail.com>
2024-03-06 23:50:28 -0300

    Update Exercicio05.java

--- tarefa03/Exercicio05.java
+++ tarefa03/Exercicio05.java
@@ -1,14 +1,13 @@
 package tarefa03;
+		/**
+ 		* Autor: Rafael Mauricio
+ 		*/
 
 import java.util.Scanner;
 
 public class Exercicio05 {
 
 	public static void main(String[] args) {
-		/**
- 		* Autor: Rafael Mauricio
- 		*/
-
 		 // Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit.
 		 // A fórmula de conversão é F = (9*C + 160)/5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
 		 
